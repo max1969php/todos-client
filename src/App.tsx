e369cf8ad0eb626253c1856eac1c2af4aab4ab2a
max1969php <90597155+max1969php@users.noreply.github.com>
2023-03-08 13:32:25 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
-import FetchTodos from './components/fetchTodos';
 import FilterSelection from './components/filterSelection';
 import { useEffect, useState } from 'react';
 import Pagination from './components/pagination';
 
 function App() {
   const [filters, setFilters]:any = useState([
-    '',false,[1,2,3,4,5,6,7,8,9,10]
-  ]);
-  const [valoriPost, setValoriPost]:any = useState([
-    '',false,[1,2,3,4,5,6,7,8,9,10]
+    '',false,''
   ]);
     
   const [data, setData] = useState([])
+  
+  const sendFilters = (valueArray:any) => {
+    setFilters(valueArray );
+    fetchData()
+    
+    console.log('USERSID layout',valueArray)
+  }
 
   let todo=[]
 
@@ -22,10 +25,10 @@ function App() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-           completedSelector:valoriPost[1],
-           usersID:valoriPost[2],
-           searchText:valoriPost[0],
-           })
+        completedSelector:filters[1],
+        usersID:filters[2],
+        searchText:filters[0],
+      })
     };
     fetch("http://localhost:3001/db/exercise", requestOptions)
       .then(response => {return response.json()})
@@ -36,89 +39,76 @@ function App() {
   fetchData()
   }, [])
 
-      const [inizio, setInizio] = useState(0);
-
-      const sendInizio = (cambio:number) => {;
-      let offset:number=0;
-      console.log(cambio);
-      if(cambio===-5 && inizio>=5)offset=-5;
-      if(cambio===-5 && inizio<5)return;
-      if(cambio===-1 && inizio>=(data.length-5))return;
-      if(cambio===-1 && inizio<(data.length-5))offset=5;
-      if(cambio>=0){setInizio(cambio*5);return};
-      setInizio(inizio +offset);
-      }
-
-
-      const returnTableData = () => {
-      
-      
-      console.log('DATI TODOS',data)
-      todo=data.slice(inizio,inizio+5)
-      console.log('new data',todo)
-      
-      return todo.map((items) => {
-        const { id,userID, title, completed } = items;
-        return (
-          <div newdata-id={id} key={id} 
-          style={{
-            display:'flex',
-            flexDirection:'row',
-            color:"black",
-            fontFamily:'karbonRegular',
-            fontSize:'16px',
-            verticalAlign:'middle',
-            zIndex:'10',
-            width: '760.5px',
-            height: '62.5px',
-            position:'relative',
-            left:'35.5px',
-            marginTop:'20px',
-            paddingTop:'10px',
-            backgroundColor: 'white',
-            borderBottom:'solid',
-            borderBottomColor:'#00A0DF'
-          }}>
-            <p
-            style={{
-              flex:'0 0 90px',
-              textAlign :'center',
-            }}>
-              {userID}</p>
-              <p
-            style={{
-              flex:'0 0 80px',
-              textAlign :'center',
-            }}></p>
-            <p
-            style={{
-              flex:'0 0 510px',
-              textAlign:'left',
-            }}>
-              {title}</p>
-            <p
-            style={{
-              flex:'0 0 90px',
-              textAlign :'center',
-            }}
-            className={'completed'+completed }>
-              </p>
-          </div>
-        )
-      });
+  const [inizio, setInizio] = useState(0);
+  
+  const sendInizio = (cambio:number) => {;
+    let offset:number=0;
+    console.log(cambio);
+    if(cambio===-5 && inizio>=5)offset=-5;
+    if(cambio===-5 && inizio<5)return;
+    if(cambio===-1 && inizio>=(data.length-5))return;
+    if(cambio===-1 && inizio<(data.length-5))offset=5;
+    if(cambio>=0){setInizio(cambio*5);return};
+    setInizio(inizio +offset);
     }
-  const sendFilters = (valueArray:any) => {
-    setFilters(valueArray );
-  setValoriPost(valueArray );
-  fetchData()
+    const returnTableData = () => {
     
-    console.log('USERSID layout',valueArray)
-  }
-const SendValoriPost = (click:any) => {
-  setValoriPost(click );
     
-    console.log('USERSID valori click',click)
+    console.log('DATI TODOS',data)
+    todo=data.slice(inizio,inizio+5)
+    console.log('new data',todo)
+    
+    return todo.map((items) => {
+      const { id,userID, title, completed } = items;
+      return (
+        <div newdata-id={id} key={id} 
+        style={{
+          display:'flex',
+          flexDirection:'row',
+          color:"black",
+          fontFamily:'karbonRegular',
+          fontSize:'16px',
+          verticalAlign:'middle',
+          zIndex:'10',
+          width: '760.5px',
+          height: '62.5px',
+          position:'relative',
+          left:'35.5px',
+          marginTop:'20px',
+          paddingTop:'10px',
+          backgroundColor: 'white',
+          borderBottom:'solid',
+          borderBottomColor:'#00A0DF'
+        }}>
+          <p
+          style={{
+            flex:'0 0 90px',
+            textAlign :'center',
+          }}>
+            {userID}</p>
+            <p
+          style={{
+            flex:'0 0 80px',
+            textAlign :'center',
+          }}></p>
+          <p
+          style={{
+            flex:'0 0 510px',
+            textAlign:'left',
+          }}>
+            {title}</p>
+          <p
+          style={{
+            flex:'0 0 90px',
+            textAlign :'center',
+          }}
+          className={'completed'+completed }>
+            </p>
+        </div>
+      )
+    });
   }
+  
   return (
     <div className='App'>
       <div className="Rettangolo888">
