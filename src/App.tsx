bda0ae48cc24c91706f26354b3c28f200b09c145
max1969php <90597155+max1969php@users.noreply.github.com>
2023-03-08 12:08:15 +0100

    ok!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import FetchTodos from './components/fetchTodos';
 import FilterSelection from './components/filterSelection';
 import { useEffect, useState } from 'react';
+import Pagination from './components/pagination';
 
 function App() {
   const [filters, setFilters]:any = useState([
@@ -11,10 +12,105 @@ function App() {
   const [valoriPost, setValoriPost]:any = useState([
     '',false,[1,2,3,4,5,6,7,8,9,10]
   ]);
+    
+  const [data, setData] = useState([])
 
+  let todo=[]
+
+  function fetchData  ()  {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+           completedSelector:valoriPost[1],
+           usersID:valoriPost[2],
+           searchText:valoriPost[0],
+           })
+    };
+    fetch("http://localhost:3001/db/exercise", requestOptions)
+      .then(response => {return response.json()})
+      .then(data => {setData(data['results'])
+    })
+  }
+  useEffect(() => {
+  fetchData()
+  }, [])
+
+      const [inizio, setInizio] = useState(0);
+
+      const sendInizio = (cambio:number) => {;
+      let offset:number=0;
+      console.log(cambio);
+      if(cambio===-5 && inizio>=5)offset=-5;
+      if(cambio===-5 && inizio<5)return;
+      if(cambio===-1 && inizio>=(data.length-5))return;
+      if(cambio===-1 && inizio<(data.length-5))offset=5;
+      if(cambio>=0){setInizio(cambio*5);return};
+      setInizio(inizio +offset);
+      }
+
+
+      const returnTableData = () => {
+      
+      
+      console.log('DATI TODOS',data)
+      todo=data.slice(inizio,inizio+5)
+      console.log('new data',todo)
+      
+      return todo.map((items) => {
+        const { id,userID, title, completed } = items;
+        return (
+          <div newdata-id={id} key={id} 
+          style={{
+            display:'flex',
+            flexDirection:'row',
+            color:"black",
+            fontFamily:'karbonRegular',
+            fontSize:'16px',
+            verticalAlign:'middle',
+            zIndex:'10',
+            width: '760.5px',
+            height: '62.5px',
+            position:'relative',
+            left:'35.5px',
+            marginTop:'20px',
+            paddingTop:'10px',
+            backgroundColor: 'white',
+            borderBottom:'solid',
+            borderBottomColor:'#00A0DF'
+          }}>
+            <p
+            style={{
+              flex:'0 0 90px',
+              textAlign :'center',
+            }}>
+              {userID}</p>
+              <p
+            style={{
+              flex:'0 0 80px',
+              textAlign :'center',
+            }}></p>
+            <p
+            style={{
+              flex:'0 0 510px',
+              textAlign:'left',
+            }}>
+              {title}</p>
+            <p
+            style={{
+              flex:'0 0 90px',
+              textAlign :'center',
+            }}
+            className={'completed'+completed }>
+              </p>
+          </div>
+        )
+      });
+    }
   const sendFilters = (valueArray:any) => {
     setFilters(valueArray );
   setValoriPost(valueArray );
+  fetchData()
     
     console.log('USERSID layout',valueArray)
   }
@@ -43,14 +139,23 @@ const SendValoriPost = (click:any) => {
           <h3 className='column3'>COMPLETED</h3>
         </div>
         <div className='Line1'></div>
-            <FetchTodos valoriPost={valoriPost}/>
+    <div>
+      <div>
+          {returnTableData()}   
+      </div>
+      
+      <div id='pager'>
+          <Pagination sendInizio={sendInizio} len={data.length}/>
+      <div>
+        <strong>{inizio}</strong>
+      </div>
+      </div>
+    </div>
       </div>
     </div>
   );
 }
 
 export default App;
-function componentDidUpdate() {
-  throw new Error('Function not implemented.');
-}
+
 
